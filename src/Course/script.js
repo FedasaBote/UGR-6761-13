d65e06dc4b5d0b4eae97564a22c802a5550b4fa4
FedasaBote <106810409+FedasaBote@users.noreply.github.com>
2023-01-15 11:31:54 +0300

    Update script.js

--- src/Course/script.js
+++ src/Course/script.js
@@ -2,7 +2,7 @@ const video = document.querySelectorAll(".video");
 const navig = document.querySelector(".navigation");
 // const container = document.querySelector(".grid-2-cols");
 const menubar = document.createElement("img");
-menubar.src = "../images/icons8-menu-rounded-50.png";
+menubar.src = "icons8-menu-rounded-50.png";
 menubar.alt = "menubar";
 const nav = navig.querySelector("nav");
 navig.append(menubar);
